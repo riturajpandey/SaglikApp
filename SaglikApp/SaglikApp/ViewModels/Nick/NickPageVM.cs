e9ba6fb7cfd36e1a55e7c4c791f85125e02406f1
Mulaym SIngh Yadav <rituraj.pandey786@gmail.com>
2020-03-05 12:15:12 +0530

    Worked on nick page

---
+++ SaglikApp/SaglikApp/ViewModels/Nick/NickPageVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SaglikApp.ViewModels.Nick
+{
+    public class NickPageVM : BaseViewModel
+    {
+        //TODO : To Define Local Class Level Variables..
+
+        #region CONSTRUCTOR
+        public NickPageVM(INavigation nav)
+        {
+            Navigation = nav;
+            NextCommand = new Command(OnNextAsync);
+        }
+        #endregion
+
+        #region COMMANDS
+        public Command NextCommand { get; set; }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// TODO : To Navigate On Next Page...
+        /// </summary>
+        private void OnNextAsync(object obj)
+        {
+           
+        }
+
+
+        #endregion
+    }
+}
---
+++ SaglikApp/SaglikApp/Views/Nick/NickPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SaglikApp.Views.Nick
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NickPage : ContentPage
+    {
+        public NickPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
